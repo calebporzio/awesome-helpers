b398da64ace629a2147df87321f95f7c472d1c08
Caleb Porzio <calebporzio@gmail.com>
2019-02-01 11:46:23 -0500

    Write-up README

--- src/helpers/str_validate.php
+++ src/helpers/str_validate.php
@@ -2,13 +2,18 @@
 
 use Illuminate\Support\Facades\Validator;
 
-function str_validate($data, $rules, $default = null)
-{
+function str_validate($data, $rules) {
     return collect(
-        Validator::make(['placeholder' => $data], ['placeholder' => $rules])
+        Validator::make(['{placeholder}' => $data], ['{placeholder}' => $rules])
             ->errors()
-            ->get('placeholder')
+            ->get('{placeholder}')
     )->map(function ($message) {
-        return str_replace('The placeholder', 'This', $message);
+        return ucfirst(
+            trim(
+                str_replace('The {placeholder}', '',
+                    str_replace('The selected {placeholder}', 'This selection', $message)
+                )
+            )
+        );
     });
 }
