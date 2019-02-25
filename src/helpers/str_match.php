e27dc8cc8cb51cb5225b0caacac6aa774e032939
MOHAMMAD ALTAWEEL <contact@mohammad.tech>
2019-02-25 12:36:02 +0300

    Check if the $pattern is a valid regex or not

--- src/helpers/str_match.php
+++ src/helpers/str_match.php
@@ -2,10 +2,8 @@
 
 function str_match($string, $pattern)
 {
-    $quotedDelimiter = preg_quote(substr($pattern, 0, 1), '#');
-
-    if (1 !== preg_match("#^($quotedDelimiter).*\\1[imsxeADSUXJu]*$#", $pattern)) {
-        $pattern = '#'.preg_quote($pattern, '#').'#';
+    if (false === @preg_match($pattern, $string)) {
+		$pattern = '#'.preg_quote($pattern, '#').'#';
     }
 
     return 1 === preg_match($pattern, $string);
