b25c8ee081ba062b8c8026d07b8cff5032baecd1
MOHAMMAD ALTAWEEL <contact@mohammad.tech>
2019-02-25 12:37:06 +0300

    Following PSR

--- src/helpers/str_match.php
+++ src/helpers/str_match.php
@@ -3,7 +3,7 @@
 function str_match($string, $pattern)
 {
     if (false === @preg_match($pattern, $string)) {
-		$pattern = '#'.preg_quote($pattern, '#').'#';
+	$pattern = '#'.preg_quote($pattern, '#').'#';
     }
 
     return 1 === preg_match($pattern, $string);
